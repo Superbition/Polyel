5579c0fe658c2ea39618e77803e53ed8704aab8b
embluk <embluk@tuta.io>
2020-12-19 16:59:11 +0000

    Add new config for the XSRF Cookie token
    Also adds config comments

--- config/session.php
+++ config/session.php
@@ -22,6 +22,7 @@ return [
     │ Set the cookie lifetime in minutes used to identify a session
     │ by Polyel. You may set this option to 0 to specify that the cookie shall
     │ expire upon session close. The default is 7 days in minutes (10080).
+    | The XSRF token cookie also uses this value for its cookie lifetime.
     │
     */
     'lifetime' => 10080,
@@ -54,7 +55,7 @@ return [
     │------------------------------------------------------------------------------
     │ The Session cookies path used to where its accessible. The default is
     │ usually the root of your application but you may change this if you need
-    │ to.
+    │ to. The XSRF token cookie also uses this value in the same way.
     │
     */
     'cookiePath' => '/',
@@ -66,6 +67,7 @@ return [
     │ This option is what is used when setting the session cookies
     │ domain option, the domain where the cookie is used to identify
     │ a session on requests.
+    | The XSRF token cookie also uses this value in the same way.
     │
     */
     'domain' => '',
@@ -77,6 +79,7 @@ return [
     │ Indicates if the Session cookie shall be only allowed over
     │ a secure connection (HTTPS). Session cookies will only be sent
     │ if a secure HTTP connection can be made when this option is set to true.
+    | The XSRF token cookie also uses this value in the same way.
     │
     */
     'secure' => false,
@@ -92,4 +95,16 @@ return [
     */
     'httpOnly' => true,
 
+
+    /*
+    │------------------------------------------------------------------------------
+    │ XSRF Protection Cookie Name
+    │------------------------------------------------------------------------------
+    │ This is a cookie that holds the CSRF token, validation is not
+    | performed using this cookie, it is meant to be used by JavaScript to
+    | make valid AJAX/HTTP requests and avoid a 401 response. Use the value of
+    | this cookie when making requests to your backend application.
+    │
+    */
+    'xsrfCookieName' => 'XSRF-TOKEN',
 ];
